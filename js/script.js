73ba90dab0e6875b876de2af56c241f7e18ff48c
Rishi Bajpai <rishirbbajpai@outlook.com>
2020-12-21 19:27:28 +0530

    added showcase section
    font customization heavily required

--- js/script.js
+++ js/script.js
@@ -38,6 +38,92 @@ $(window).on("scroll", function() {
         }
 });
 
-ScrollReveal().reveal('.lottiefile');
-ScrollReveal().reveal('.section-head', { delay: 500 });
-ScrollReveal().reveal('.section-text', { delay: 500 });
+ScrollReveal().reveal('.para');
+ScrollReveal().reveal('.hero-message', { delay: 500 });
+ScrollReveal().reveal('.head', { delay: 500 });
+ScrollReveal().reveal('.lottiefile', { delay: 500 });
+ScrollReveal().reveal('.section-head', { delay: 800 });
+ScrollReveal().reveal('.section-text', { delay: 1200 });
+ScrollReveal().reveal('#girl');
+ScrollReveal().reveal('#girlshadow', { delay: 500 });
+
+
+$('.card-carousel').slick({
+    infinite: true,
+    speed: 300,
+    centerPadding: '60px',
+    variableWidth: true,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToScroll: 1,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+  });
+
+  function detect_active(){
+    // get active
+    var get_active = $("#dp-slider .dp_item:first-child").data("class");
+    $("#dp-dots li").removeClass("active");
+    $("#dp-dots li[data-class="+ get_active +"]").addClass("active");
+  }
+  $("#dp-next").click(function(){
+    var total = $(".dp_item").length;
+    $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function (index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+    detect_active();
+
+  });
+
+  $("#dp-prev").click(function(){
+    var total = $(".dp_item").length;
+    $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function (index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+
+    detect_active();
+  });
+
+  $("#dp-dots li").click(function(){
+    $("#dp-dots li").removeClass("active");
+    $(this).addClass("active");
+    var get_slide = $(this).attr('data-class');
+    console.log(get_slide);
+    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function (index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+  });
+
+
+  $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function(){
+    var get_slide = $(this).attr('data-class');
+    console.log(get_slide);
+    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function (index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+
+    detect_active();
+  });
