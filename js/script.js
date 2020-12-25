3b0c17392dd454833124762f593beeb48ecc00f8
Rishi Bajpai <46041307+iamrishirb@users.noreply.github.com>
2020-12-25 22:14:38 +0530

    some optimisations done

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 var typed = new Typed('#slider', {
-    strings: ["UI Components", "Website Templates", "UI Assets", "Design Resources", "UI Snippets"],
-    typeSpeed: 100,
-    backSpeed: 40,
-    showCursor: true,
-    loop: true,
-    cursorChar: '_',
-    backDelay: 1000
+  strings: ["UI Components", "Website Templates", "UI Assets", "Design Resources", "UI Snippets"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  showCursor: true,
+  loop: true,
+  cursorChar: '_',
+  backDelay: 1000
 });
 
-
+//mobile navigation
 function openNav() {
-    document.getElementById("main-nav").style.width = "100%";
-  }
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("main-nav").style.width = "0";
-  }
-  
+document.getElementById("main-nav").style.width = "100%";
+$("#floating-bg").addClass("displayNone");
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+document.getElementById("main-nav").style.width = "0";
+$("#floating-bg").removeClass("displayNone");
+}
 
 // Menu-toggle button
-
 $(document).ready(function() {
-        $("#menu-icon").on("click", function() {
-            $("#menu ul").toggleClass("showing");
-        });
+      $("#menu-icon").on("click", function() {
+          $("#menu ul").toggleClass("showing");
+      });
 });
 
 // Scrolling Effect
-
 $(window).on("scroll", function() {
         if($(window).scrollTop()) {
             $('#navigation').addClass('black');
@@ -48,82 +47,51 @@ ScrollReveal().reveal('#girl');
 ScrollReveal().reveal('#girlshadow', { delay: 500 });
 
 
-$('.card-carousel').slick({
-    infinite: true,
-    speed: 300,
-    centerPadding: '60px',
-    variableWidth: true,
-    slidesToShow: 4,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 1
-          }
-        }
-      ]
+function detect_active(){
+  // get active
+  var get_active = $("#dp-slider .dp_item:first-child").data("class");
+  $("#dp-dots li").removeClass("active");
+  $("#dp-dots li[data-class="+ get_active +"]").addClass("active");
+}
+$("#dp-next").click(function(){
+  var total = $(".dp_item").length;
+  $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+  $.each($('.dp_item'), function (index, dp_item) {
+    $(dp_item).attr('data-position', index + 1);
+  });
+  detect_active();
+
+});
+
+$("#dp-prev").click(function(){
+  var total = $(".dp_item").length;
+  $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+  $.each($('.dp_item'), function (index, dp_item) {
+    $(dp_item).attr('data-position', index + 1);
   });
 
-  function detect_active(){
-    // get active
-    var get_active = $("#dp-slider .dp_item:first-child").data("class");
-    $("#dp-dots li").removeClass("active");
-    $("#dp-dots li[data-class="+ get_active +"]").addClass("active");
-  }
-  $("#dp-next").click(function(){
-    var total = $(".dp_item").length;
-    $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-    detect_active();
+  detect_active();
+});
 
+$("#dp-dots li").click(function(){
+  $("#dp-dots li").removeClass("active");
+  $(this).addClass("active");
+  var get_slide = $(this).attr('data-class');
+  console.log(get_slide);
+  $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+  $.each($('.dp_item'), function (index, dp_item) {
+    $(dp_item).attr('data-position', index + 1);
+  });
+});
+
+
+$("body").on("click", "#dp-slider .dp_item:not(:first-child)", function(){
+  var get_slide = $(this).attr('data-class');
+  console.log(get_slide);
+  $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+  $.each($('.dp_item'), function (index, dp_item) {
+    $(dp_item).attr('data-position', index + 1);
   });
 
-  $("#dp-prev").click(function(){
-    var total = $(".dp_item").length;
-    $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-
-    detect_active();
-  });
-
-  $("#dp-dots li").click(function(){
-    $("#dp-dots li").removeClass("active");
-    $(this).addClass("active");
-    var get_slide = $(this).attr('data-class');
-    console.log(get_slide);
-    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-  });
-
-
-  $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function(){
-    var get_slide = $(this).attr('data-class');
-    console.log(get_slide);
-    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
-    $.each($('.dp_item'), function (index, dp_item) {
-      $(dp_item).attr('data-position', index + 1);
-    });
-
-    detect_active();
-  });
+  detect_active();
+});
